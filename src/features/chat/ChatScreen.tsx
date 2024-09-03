36f7b3dbc40e7265728d450532c4f5a724b864a4
Banid Azin <banidazin33@gmail.com>
2024-09-03 19:07:37 +0530

    Add react navigation and setup basic bottom tab navigation

---
+++ src/features/chat/ChatScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const ChatScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>ChatScreen</Text>
+    </View>
+  );
+};
---
+++ src/router.js
@@ -0,0 +1,41 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { ChatScreen } from "./features/chat";
+import { DummyScreen } from "./features/dummyTab";
+
+const Tab = createBottomTabNavigator();
+
+export const Router = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "chat") {
+              iconName = focused ? "chatbubble" : "chatbubble-outline";
+            } else if (route.name === "dummy") {
+              iconName = focused ? "list" : "list-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "green",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="chat"
+          component={ChatScreen}
+          options={{
+            title: "Recipes",
+          }}
+        />
+        <Tab.Screen name="dummy" component={DummyScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
