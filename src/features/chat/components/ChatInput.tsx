1fed74925a86f119bf87cec98ebb52bf4af6f87c
Banid Azin <banidazin33@gmail.com>
2024-09-04 00:58:16 +0530

    created a text input

--- src/features/chat/components/ChatInput.tsx
+++ src/features/chat/components/ChatInput.tsx
@@ -1,13 +1,53 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 export const ChatInput: React.FC = () => {
   const [inputMessage, setInputMessage] = useState("");
+  const translatePositionX = useSharedValue(0);
 
   const onInputChange = (text: string) => {
     setInputMessage(text);
+    if (text.length === 0 && translatePositionX.value === 0) return;
+    if (text.length > 0 && translatePositionX.value === 0) {
+      translatePositionX.value = withTiming(1, { duration: 500 });
+      return;
+    }
+    if (text.length === 0 && translatePositionX.value === 1) {
+      translatePositionX.value = withTiming(0, { duration: 500 });
+      return;
+    }
   };
+
+  const handleSendMessage = () => {};
+
+  const micButtonStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(translatePositionX.value, [0, 1], [0, 100]);
+    return {
+      transform: [
+        {
+          translateX,
+        },
+      ],
+    };
+  });
+
+  const sendButtonStyle = useAnimatedStyle(() => {
+    const scale = interpolate(translatePositionX.value, [0, 1], [0, 1]);
+    return {
+      transform: [
+        {
+          scale,
+        },
+      ],
+    };
+  });
 
   return (
     <View style={styles.container}>
@@ -20,8 +60,17 @@ export const ChatInput: React.FC = () => {
           style={styles.input}
         />
       </View>
-      <View style={styles.micIconContainer}>
-        <Ionicons name="mic" size={20} color="white" />
+      <View style={{ flexDirection: "row" }}>
+        <Animated.View style={[styles.sendButtonContainer, sendButtonStyle]}>
+          <TouchableOpacity style={styles.sendButton}>
+            <Ionicons name="send" size={20} color="white" />
+          </TouchableOpacity>
+        </Animated.View>
+        <Animated.View style={[micButtonStyle]}>
+          <TouchableOpacity style={styles.micButton}>
+            <Ionicons name="mic" size={20} color="white" />
+          </TouchableOpacity>
+        </Animated.View>
       </View>
     </View>
   );
@@ -47,10 +96,20 @@ const styles = StyleSheet.create({
     maxHeight: 120,
     color: "white",
   },
-  micIconContainer: {
+  sendButtonContainer: {
+    position: "absolute",
+  },
+  sendButton: {
     alignItems: "center",
     justifyContent: "flex-end",
     padding: 10,
     borderRadius: 10,
+    backgroundColor: "green",
+  },
+  micButton: {
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 30,
   },
 });
